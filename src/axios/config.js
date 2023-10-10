df77ac775d464c5803d47374468b298dc8e5da19
erick <michel172002@gmail.com>
2023-10-09 23:16:06 -0300

    FEAT: Configurando Axios para enviar o Token JWT nos request e mudando o URL base da API.

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,10 +1,20 @@
 import axios from "axios"
 
 const sbk4Fetch = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://sbk4.api.japa.boo",
     headers: {
         "Content-Type": "application/json",
     }
+})
+
+sbk4Fetch.interceptors.request.use((config) => {
+  //const token = localStorage.getItem('token')
+
+  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlzcyI6IkFQSSBTQks0IiwiZXhwIjoxNjk2OTEzODE4fQ.BlBomnlElG4W39iwf_j6h8ctBYEXfDMDyE63CAPioBE'
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
 })
 
 export default sbk4Fetch
