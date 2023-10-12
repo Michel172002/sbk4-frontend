5052f23bb04cb314a455b2628c98843c478d57a4
Douglas Vidal <vidaldl01@gmail.com>
2023-10-12 19:26:12 -0300

    header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,7 +8,7 @@ if(page=="/"){
 var pageReplace=page.replace(/^\//,"")
     return(
         <div>
-            <Containner>
+            <Containner className="shadow d-flex align-items-center justify-content-center">
                 <h3>{pageReplace}</h3>
             </Containner>
         </div>
--- src/components/header/styled.js
+++ src/components/header/styled.js
@@ -8,12 +8,17 @@ height:80px;
 text-align: center;
 margin-top:12vh;
 margin-left: 28%;
-background-color:#0F1A2C;
+background: linear-gradient(45deg, #0F1A2C, #1E2F46);
+color: #CACACA;
+border-radius: 10px;
 
 h3{
     color:white;
     font-size: 40px;
-    
+}
+
+h3::first-letter {
+  text-transform: uppercase;
 }
 
 `
